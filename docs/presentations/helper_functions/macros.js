7df484b0528d10f5babc6e2a194b5bad4a929fda
Gabriel Geisler Mesevage <gabriel.mesevage@gmail.com>
2020-08-21 18:15:48 +0300

    removed the presentations folder

--- docs/presentations/helper_functions/macros.js
+++
@@ -1,8 +0,0 @@
-remark.macros.scale = function(w) {
-  var url = this;
-  return '<img src="' + url + '" style="width: ' + w + '" />';
-};
-
-.large { font-size: 130% };
-
-.small { font-size: 70% };
